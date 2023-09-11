65cb69be957a8995e91302a9293e185352f217cd
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-09-11 14:59:11 +0200

    add title and img

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,9 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        
+        title: "Vue Hello",
+        img: "https://picsum.photos/200/300",
+        orangeText: "orange"
       }
     }
-  }).mount('#app')
+   }).mount('#app')
